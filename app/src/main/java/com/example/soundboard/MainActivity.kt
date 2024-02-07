1d274ab033afcb4d02e6cf782b6ed4a01c723ba2
Jandrus000 <jcandrus02@gmail.com>
2024-02-06 19:50:29 -0700

    added audio and template is finished

--- app/src/main/java/com/example/soundboard/MainActivity.kt
+++ app/src/main/java/com/example/soundboard/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.soundboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.widget.Button
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,26 +16,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.soundboard.ui.theme.SoundBoardTheme
 
 class MainActivity : ComponentActivity() {
+    fun playSound(path: String){
+        var mp = MediaPlayer()
+        mp.setDataSource(
+            this,
+            Uri.parse(path)
+        )
+        mp.prepare()
+        mp.start()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        println("Successfully set layout")
+
+        var marineButton = findViewById<Button>(R.id.marines)
+
+        println("Objects created")
+        marineButton.setOnClickListener {
+            println("Inside click listener")
+            playSound("android.resource://" + this.packageName + "/" + R.raw.marines_audio)
+
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SoundBoardTheme {
-        Greeting("Android")
     }
+
 }
